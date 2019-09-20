395f1b444c169083b6479193de09d590cf8c35e6
Tom Cobb <tom.cobb@diamond.ac.uk>
2019-09-20 15:35:20 +0100

    Added start of a cli

--- tests/test_versiongit.py
+++ tests/test_versiongit.py
@@ -1,28 +1,46 @@
+import os
+import shutil
 import sys
 from subprocess import check_output
 from tempfile import mkdtemp
 
+import versiongit
 
-def checkout_repo_and_import(commit=None):
-    # Check the current remotes, and checkout from the first
-    d = mkdtemp()
-    remotes = check_output("git remote -v".split()).decode()
-    first_remote = remotes.splitlines()[0].split()[1]
-    clone_command = "git clone"
-    if commit:
-        clone_command += " --branch %s" % commit
-    check_output(("%s %s %s" % (clone_command, first_remote, d)).split())
-    sys.path.insert(1, d)
-    import versiongit
-    return d, versiongit
+
+class TempRepo:
+    def __init__(self, commit):
+        # Check the current remotes, and checkout from the first
+        self.dir = mkdtemp()
+        remotes = check_output("git remote -v".split()).decode()
+        first_remote = remotes.splitlines()[0].split()[1]
+        clone_command = "git clone --branch %s %s %s" % (
+            commit, first_remote, self.dir)
+        check_output(clone_command.split())
+
+    def version(self):
+        script = os.path.join(self.dir, "versiongit", "command.py")
+        version = check_output([sys.executable, script, "--version"])
+        return version
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        #shutil.rmtree(self.dir)
+        pass
 
 
 def test_current_version_exists_and_is_str():
-    import versiongit
     assert isinstance(versiongit.__version__, str)
 
 
 def test_tagged_version():
-    d, versiongit = checkout_repo_and_import("0.1")
-    assert versiongit.__version__ == "0.1"
+    with TempRepo("0.1") as repo:
+        assert repo.version() == "0.1"
 
+
+def test_tagged_version_with_modification():
+    with TempRepo("0.1") as repo:
+        with open(os.path.join(repo.dir, "versiongit", "__init__.py"), "a") as f:
+            f.write("\n")
+        assert repo.versiongit.__version__ == "0.1"
---
+++ versiongit/command.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from argparse import ArgumentParser
+
+
+def cli():
+    parser = ArgumentParser(
+        description="Command line tool adding versiongit to python module")
+    parser.add_argument("--version", action="store_true",
+                        help="Print the current version of versiongit")
+    args = parser.parse_args()
+    if args.version:
+        sys.path.insert(1, os.path.join(os.path.dirname(__file__), ".."))
+        from versiongit import __version__
+        print(__version__)
+
+
+if __name__ == "__main__":
+    cli()
